a661e928735a6ff42dd656eac070800d909a7bf2
SaeGyo <wjstpry@naver.com>
2024-06-27 12:41:45 +0900

    03.js, 03.css, index.css

---
+++ 03/03.js
@@ -0,0 +1,21 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//     console.log('안녕!!')
+// }) ;
+document.addEventListener("DOMContentLoaded", ()=>{
+    console.log('화살표 함수!!')
+
+    //변수 선언
+    // const msg = document.getElementById('msg') ;
+    const msg = document.querySelector('#msg') ;
+    const bt = document.querySelector('button') ;
+
+    // msg.innerHTML = '<h1>안녕</h1>' ;
+    // msg.innerText = '<h1>안녕</h1>' ;
+    // innerHTML은 태그를 번역해줌.
+    // innerTEXT는 문자열을 그대로 출력함.
+
+    bt.addEventListener("click", ()=>{
+        if (msg.innerHTML == '안녕' ;) msg.innerHTML == '잘가~~' ;
+        else msg.innerHTML = "안녕" ;
+    });
+});
